e7fb5e81cae7e1d0a472e7ee1fab5235ba0c8fdd
Lance Dang <lancedang64@gmail.com>
2021-02-23 19:27:51 +1100

    completed resetGrid function; modified clearOldGrid, getSquaresPerSide, createNewGrid funcitons; added new functions: changeGridTemplate and attachEventListenerToSquares. Project marked finished.

--- myscript.js
+++ myscript.js
@@ -4,23 +4,55 @@ function fillColor(e) {
     e.srcElement.style.backgroundColor = "black";
 }
 
-const CONTAINER_DIV = document.getElementById("container");
-
-for (let i = 0; i < 16*16; i++) {
-    let squareDiv = document.createElement("div");
-    squareDiv.id = "gridSquare";
-    CONTAINER_DIV.appendChild(squareDiv);
+function resetGrid(e) {
+    clearOldGrid();
+    const SQUARES_PER_SIDE = getSquaresPerSide();
+    changeGridTemplate(SQUARES_PER_SIDE);
+    createNewGrid(SQUARES_PER_SIDE);
 }
 
+function clearOldGrid() {
+    const GRID_SQUARES = document.querySelectorAll("#gridSquare");
+    GRID_SQUARES.forEach(square => square.remove());
+}
 
-let gridSquares = document.querySelectorAll("#gridSquare");
-gridSquares.forEach(square => {
-    square.addEventListener("mouseover", fillColor);
-});
+function getSquaresPerSide() {
+    let squaresPerSide;
+    do { 
+        squaresPerSide = window.prompt("How many squares would you like per side for the new grid?");
+        if (squaresPerSide == null) break;
+        squaresPerSide = parseInt(squaresPerSide);
+        if (squaresPerSide < 0 || squaresPerSide > 100) window.alert("The ammount must be between 0 and 100");
+    } while (!Number.isInteger(squaresPerSide) || squaresPerSide < 0 || squaresPerSide > 100);
+    return squaresPerSide;
+}
 
-/*
-const testButton = document.querySelector("#testButton");
-testButton.addEventListener("click", function() {
-    fillColor();
-});
-*/
+function changeGridTemplate(squaresPerSide) {
+    document.getElementById("container").style.gridTemplateColumns = `repeat(${squaresPerSide}, auto)`;
+    document.getElementById("container").style.gridTemplateRows = `repeat(${squaresPerSide}, auto)`;
+}
+
+function createNewGrid(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide*squaresPerSide; i++) {
+        let squareDiv = document.createElement("div");
+        squareDiv.id = "gridSquare";
+        CONTAINER_DIV.appendChild(squareDiv);
+    }
+    attachEventListenerToSquares();
+}
+
+function attachEventListenerToSquares() {
+    const GRID_SQUARES = document.querySelectorAll("#gridSquare");
+    GRID_SQUARES.forEach(square => {
+        square.addEventListener("mouseover", fillColor);
+    });
+}
+
+const CONTAINER_DIV = document.getElementById("container");
+
+const RESET_GRID_BUTTON = document.querySelector("#resetGridButton");
+RESET_GRID_BUTTON.addEventListener("click", resetGrid);
+
+createNewGrid(16);
+
+//the problem now is that the grid layout is not dynamic, cant create squares everytime because the grid template is 16*16 by defaulft
